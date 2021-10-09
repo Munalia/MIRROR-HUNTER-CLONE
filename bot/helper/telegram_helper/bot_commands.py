a4211f3aae2d22f7e50f7a733fd694a83816d31b
ZekXtreme <83297638+shadowhunter777@users.noreply.github.com>
2021-10-09 07:08:27 +0545

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -1,21 +1,31 @@
+import os
+
+def getCommand(name: str, command: str):
+    try:
+        if len(os.environ[name]) == 0:
+            raise KeyError
+        return os.environ[name]
+    except KeyError:
+        return command
+
 class _BotCommands:
     def __init__(self):
-        self.StartCommand = 'start'
-        self.CancelMirror = 'cancel'
-        self.CancelAllCommand = 'cancelall'
-        self.ListCommand = 'list'
-        self.StatusCommand = 'status'
-        self.AuthorizedUsersCommand = 'users'
-        self.AuthorizeCommand = 'auth'
-        self.UnAuthorizeCommand = 'unauth'
-        self.AddSudoCommand = 'addsudo'
-        self.RmSudoCommand = 'rmsudo'
-        self.RestartCommand = 'restart'
-        self.StatsCommand = 'stats'
-        self.HelpCommand = 'help'
-        self.LogCommand = 'log'
-        self.CloneCommand = 'clone'
-        self.CountCommand = 'count'
-        self.DeleteCommand = 'del'
-        
+        self.StartCommand = getCommand('START_BOT', 'start')
+        self.CancelMirror = getCommand('CANCEL_BOT', 'cancel')
+        self.CancelAllCommand = getCommand('CANCEL_ALL_BOT', 'cancelall')
+        self.ListCommand = getCommand('LIST_BOT', 'list')
+        self.StatusCommand = getCommand('STATUS_BOT', 'status')
+        self.AuthorizedUsersCommand = getCommand('USERS_BOT', 'users')
+        self.AuthorizeCommand = getCommand('AUTH_BOT', 'auth')
+        self.UnAuthorizeCommand = getCommand('UNAUTH_BOT', 'unauth')
+        self.AddSudoCommand = getCommand('ADDSUDO_BOT', 'addsudo')
+        self.RmSudoCommand = getCommand('RMSUDO_BOT', 'rmsudo')
+        self.RestartCommand = getCommand('RESTART_BOT', 'restart')
+        self.StatsCommand = getCommand('STATS_BOT', 'stats')
+        self.HelpCommand = getCommand('HELP_BOT', 'help')
+        self.LogCommand = getCommand('LOG_BOT', 'logs')
+        self.CloneCommand = getCommand('CLONE_BOT', 'clone')
+        self.CountCommand = getCommand('COUNT_BOT', 'count')
+        self.DeleteCommand = getCommand('DELETE_BOT', 'del')
+
 BotCommands = _BotCommands()
